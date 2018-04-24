9f6627ece26e91cf58b4f13c0e74a169b0ec85ff
sakditad2542 <34784504+sakditad2542@users.noreply.github.com>
2018-04-24 22:33:42 +0700

    Add files via upload

---
+++ logoDebian.sh
@@ -0,0 +1,8 @@
+# set logo debian
+
+cd
+wget https://github.com/KittyKatt/screenFetch/raw/master/screenfetch-dev
+mv screenfetch-dev /usr/bin/screenfetch
+chmod +x /usr/bin/screenfetch
+echo "clear" >> .profile
+echo "screenfetch" >> .profile
